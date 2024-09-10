47bc6f81d1613c5159c5897cb43677897acf45b5
Tyler French <tyler.r.french@gmail.com>
2024-09-10 11:29:17 -0400

    modified constants.py adding player_radius, main.py drawing player class, adding circleshape.py to call circle class, and player.py drawing player triangle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 
 
@@ -11,15 +13,30 @@ def game_start():
 
 def main():
 	pygame.init()
-	print(f"pygame live: {pygame.get_init()}")
+	# print(f"pygame live: {pygame.get_init()}")
+	
+	Clock = pygame.time.Clock
+	dt = 0
+	
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	game_start()
+
+	x = SCREEN_WIDTH / 2
+	y = SCREEN_HEIGHT / 2
+
+	player = Player(x, y)
+	
 	while pygame.get_init() == True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill(color=(0, 0, 0, 1))
+		player.draw(screen)
 		pygame.display.flip()
+
+		
+
+		dt = Clock().tick(60) / 1000  # Last action of frame
 
 
 
